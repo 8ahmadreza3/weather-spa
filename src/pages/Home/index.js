9c22bbe489eb4740609b255b823085afaca84036
8ahmadreza3 <8arhp3@gmail.com>
2024-05-30 17:35:56 +0330

    cities request

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+/* eslint-disable array-callback-return */
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import City from '../../components/CityCard/City.js'
 
 export default function Home() {
+  const [cities, setCities] = useState([])
+
+  useEffect(()  => {
+    fetch('http://localhost:8000/cities?capital=admin')
+      .then(res =>
+        res.json()
+      ).then(data => setCities(data))
+  }, [])
+  console.log(cities);
+
   return (
-    <City/>
+    <div className='d-flex bg-primary'>
+      {cities?.map((city) => {
+        <City {...city} />
+      })}
+    </div>
   )
 }
