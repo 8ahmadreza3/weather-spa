015f95ed5d567e43e147acd39633b2fc83b2c41a
8ahmadreza3 <8arhp3@gmail.com>
2024-06-07 15:40:35 +0330

    page not found

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './NotFound.css'
+import { Link } from 'react-router-dom';
+import { Image } from 'react-bootstrap';
+import badWeather from './—Pngtree—ve_3773496.png'
 
 function NotFound() {
-  return ( 
+  return (
     <>
-      <div className=''>
-        404
+      <div className='d-flex justify-content-center'>
+        <div className='pt-5'>
+          <p className='notfound'>404</p>
+        </div>
+        <div className=''>
+          <Image className='bad-weather' src={badWeather} />
+        </div>
+      </div>
+      <div className='d-flex justify-content-center'>
+        <p className='fs-1 m-auto'>Page Not Found !!!</p>
+      </div>
+      <div className='d-flex justify-content-center mt-5'>
+        <Link className='btn btn-primary' to='/'>Go To HomePage</Link>
       </div>
     </>
   );
